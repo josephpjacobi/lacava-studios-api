368d6de9e4a309ae0b5dd967e609ebb087f24f6a
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-11-08 23:14:33 -0500

    added express and test endpoint to app and imported app var to app file

---
+++ api/src/app.ts
@@ -0,0 +1,16 @@
+import { Application, Request, Response } from "express";
+// import { db } from "./db";
+import express from "express";
+// import cors from "cors";
+
+export const app: Application = express();
+
+// app.use(cors());
+
+app.get("/products", async (req: Request, res: Response) => {
+	try {
+		res.send("Hello World");
+	} catch (error) {
+		console.log("Error processing your request:", error);
+	}
+});
---
+++ api/src/server.ts
@@ -0,0 +1,6 @@
+import { app } from "./app";
+const port = 8080;
+
+app.listen(port, () => {
+	console.log(`server started at http://localhost:${port}`);
+});
