4683d83608c1293ffc06cfb41d4b9f84d0472559
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-11-12 10:49:31 -0500

    added to schema file

--- database/schema.sql
+++ database/schema.sql
@@ -1,50 +1,60 @@
-CREATE TABLE products (
-  id serial not null, 
-  name text,
-  description text,
-  price integer,
-  orderable boolean,
-  forSale boolean,
-  year_completed int,
-  created_at timestamp with time zone, 
-  updated_at timestamp with time zone,
-  deleted_at timestamp with time zone,
+CREATE SCHEMA lacava_studios;
+
+CREATE TABLE lacava_studios.products (
+  id SERIAL PRIMARY KEY NOT NULL, 
+  name TEXT,
+  description TEXT,
+  price INTEGER,
+  orderable BOOLEAN,
+  forSale BOOLEAN,
+  year_completed INTEGER,
+  created_at TIMESTAMP with time zone, 
+  updated_at TIMESTAMP with time zone,
+  deleted_at TIMESTAMP with time zone,
+  -- PRIMARY KEY id
 );
 
-CREATE TABLE orders (
-  id serial not null,
-  status text,
-  buyerName text,
-  buyerEmail text,
+CREATE TABLE lacava_studios.orders (
+  id serial PRIMARY KEY NOT NULL,
+  status TEXT,
+  buyerName TEXT,
+  buyerEmail TEXT,
   buyerAddress object,  -- Can this be an object?
   products object, -- array of product id's
-  total integer
+  total INTEGER
+  -- PRIMARY KEY id
 )
 
-CREATE TABLE product_views (
-  id serial not null, 
-  product_id integer, -- fk
-  created_at timestamp with time zone,
+CREATE TABLE lacava_studios.product_views (
+  id serial NOT NULL, 
+  product_id INTEGER, -- fk
+  created_at TIMESTAMP with time zone,
+  FOREIGN KEY(product_id) REFERENCES products(id)
+  -- PRIMARY KEY id
 )
 
-CREATE TABLE pictures (
-  id serial not null,
-  product_id integer, --fk
-  alt text, -- This will be shown to a screen reader
-  small_url text,
-  med_url text, -- different sized urls for different mesia types
-  large_url text,
-  created_at timestamp with time zone,
-  updated_at timestamp with time zone
+CREATE TABLE lacava_studios.pictures (
+  id serial NOT NULL,
+  product_id INTEGER, --fk
+  alt TEXT, -- This will be shown to a screen reader
+  small_url TEXT,
+  med_url TEXT, -- different sized urls for different mesia types
+  large_url TEXT,
+  created_at TIMESTAMP with time zone,
+  updated_at TIMESTAMP with time zone,
+  FOREIGN KEY(product_id) REFERENCES products(id)
+  -- PRIMARY KEY id
 )
 
-CREATE TABLE process_videos (
-  id serial not null,
-  product_id integer, -- fk
-  alt text, -- This will be shown to a screen reader
-  small_url text,
-  med_url text, -- different sized urls for different mesia types
-  large_url text,
-  created_at timestamp with time zone,
-  updated_at timestamp with time zone
+CREATE TABLE lacava_studios.process_videos (
+  id serial NOT NULL,
+  product_id INTEGER, -- fk
+  alt TEXT, -- This will be shown to a screen reader
+  small_url TEXT,
+  med_url TEXT, -- different sized urls for different mesia types
+  large_url TEXT,
+  created_at TIMESTAMP with time zone,
+  updated_at TIMESTAMP with time zone,
+  FOREIGN KEY(product_id) REFERENCES products(id)
+  -- PRIMARY KEY id
 )
