174253c690e1ca3bdd249658066d740bd2184d93
Joe Jacobi <joseph.p.jacobi@gmail.com>
2021-11-14 11:58:31 -0500

    updated schema, commented out unneeded tables

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,8 @@
-CREATE SCHEMA IF NOT EXISTS lacava_studios;
+-- CREATE SCHEMA IF NOT EXISTS lacava_studios;
 
-CREATE TABLE lacava_studios.products (
-  id SERIAL NOT NULL PRIMARY KEY, 
+-- CREATE TABLE lacava_studios.products (
+CREATE TABLE IF NOT EXISTS products (
+  id SERIAL NOT NULL, 
   name TEXT,
   description TEXT,
   price INTEGER,
@@ -10,49 +11,58 @@ CREATE TABLE lacava_studios.products (
   year_completed INTEGER,
   created_at TIMESTAMP with time zone, 
   updated_at TIMESTAMP with time zone,
-  deleted_at TIMESTAMP with time zone
+  deleted_at TIMESTAMP with time zone,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS lacava_studios.orders (
-  id serial NOT NULL PRIMARY KEY,
+-- CREATE TABLE IF NOT EXISTS lacava_studios.orders (
+CREATE TABLE IF NOT EXISTS orders (
+  id serial NOT NULL,
   status TEXT,
   buyerName TEXT,
   buyerEmail TEXT,
   buyerAddress TEXT,  -- Can this be an object?
   products TEXT, -- array of product id's
   total INTEGER
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS lacava_studios.product_views (
-  id serial NOT NULL PRIMARY KEY, 
+-- CREATE TABLE IF NOT EXISTS lacava_studios.product_views (
+CREATE TABLE IF NOT EXISTS product_views (
+  id serial NOT NULL, 
   product_id INTEGER, -- fk
   created_at TIMESTAMP with time zone
+  PRIMARY KEY(id)
+  CONSTRAINT fk_product
+    FOREIGN KEY(product_id)
+      REFERENCES product(id)
   -- FOREIGN KEY(product_id) REFERENCES product(id)
 );
 
-CREATE TABLE IF NOT EXISTS lacava_studios.pictures (
-  id serial NOT NULL PRIMARY KEY,
-  product_id INTEGER, --fk
-  alt TEXT, -- This will be shown to a screen reader
-  small_url TEXT,
-  med_url TEXT, -- different sized urls for different mesia types
-  large_url TEXT,
-  created_at TIMESTAMP with time zone,
-  updated_at TIMESTAMP with time zone
-  -- FOREIGN KEY(product_id) REFERENCES products(id)
-);
+-- CREATE TABLE IF NOT EXISTS lacava_studios.pictures (
+-- CREATE TABLE IF NOT EXISTS pictures (
+--   id serial NOT NULL PRIMARY KEY,
+--   product_id INTEGER, --fk
+--   alt TEXT, -- This will be shown to a screen reader
+--   small_url TEXT,
+--   med_url TEXT, -- different sized urls for different mesia types
+--   large_url TEXT,
+--   created_at TIMESTAMP with time zone,
+--   updated_at TIMESTAMP with time zone
+--   -- FOREIGN KEY(product_id) REFERENCES products(id)
+-- );
 
-CREATE TABLE IF NOT EXISTS lacava_studios.process_videos (
-  id serial NOT NULL PRIMARY KEY,
-  product_id INTEGER, -- fk
-  alt TEXT, -- This will be shown to a screen reader
-  small_url TEXT,
-  med_url TEXT, -- different sized urls for different mesia types
-  large_url TEXT,
-  created_at TIMESTAMP with time zone,
-  updated_at TIMESTAMP with time zone
-  -- FOREIGN KEY(product_id) REFERENCES products(id)
-);
+-- CREATE TABLE IF NOT EXISTS lacava_studios.process_videos (
+--   id serial NOT NULL PRIMARY KEY,
+--   product_id INTEGER, -- fk
+--   alt TEXT, -- This will be shown to a screen reader
+--   small_url TEXT,
+--   med_url TEXT, -- different sized urls for different mesia types
+--   large_url TEXT,
+--   created_at TIMESTAMP with time zone,
+--   updated_at TIMESTAMP with time zone
+--   -- FOREIGN KEY(product_id) REFERENCES products(id)
+-- );
 
 
 -- alter table product_views add foreign key (product_id) REFERENCES products(id);
